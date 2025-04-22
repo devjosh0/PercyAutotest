08b4e03d0513d3ae6df1523272a0f42ef8ca48f1
devjosh0 <husseinamadu634@gmail.com>
2025-04-22 09:01:07 +0000

    single element on percy

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,13 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://amalitech-website-web.vercel.app/testing-space')
-    cy.document().percySnapshot('Docs');
+    cy.url().should('include', 'testing-space')
+   cy.document().percySnapshot('Docs');
+   
+   cy.get('section.w-full.text-white.py-6.px-2xl.md\\:px-5.bg-white').should('be.visible');
+cy.percySnapshot('Block Quote', {
+  scope: 'section.w-full.text-white.py-6.px-2xl.md\\:px-5.bg-white'
+});
+
   })
 })
